b2395911cdd1ec984f7266aa182de0e7d5ba72ab
Hakim Ghods <ghods.hakim@hotmail.com>
2020-11-16 11:02:45 +0330

    Init Users

---
+++ MyApp/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime Birthdate { get; set; }
+    }
+}
